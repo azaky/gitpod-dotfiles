a56083cbb7985110dde0391e70251bf69c30e1bb
Ahmad Zaky <ahmadzaky003@gmail.com>
2022-06-12 04:40:32 +0000

    add install doppler

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ git config --global alias.s status
 
 # Install air
 go install github.com/cosmtrek/air@latest
+
+# Install doppler
+# https://docs.doppler.com/docs/install-cli
+(curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh || wget -t 3 -qO- https://cli.doppler.com/install.sh) | sudo sh
