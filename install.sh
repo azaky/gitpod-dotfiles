cc8d5c65050b9bdf6e70f632fb9160fee4a8fcb7
Ahmad Zaky <ahmadzaky003@gmail.com>
2022-06-29 11:27:28 +0000

    add install migrate

--- install.sh
+++ install.sh
@@ -12,3 +12,6 @@ npm i -g serve
 # Install doppler
 # https://docs.doppler.com/docs/install-cli
 (curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh || wget -t 3 -qO- https://cli.doppler.com/install.sh) | sudo sh
+
+# Install migrate
+go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
